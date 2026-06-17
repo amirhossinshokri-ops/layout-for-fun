fc5b059f912107d28630ccdb993eef56ddf93201
ash <amirhossin.shokri@gmail.com>
2026-06-17 16:34:11 +0330

    complete destenation style

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,22 @@
 const btn=document.querySelector('.navbtn')
 const btnLine=document.querySelector('.navebtn__line')
 const mobile_menu=document.querySelector('.mobile_menu')
+
+const hearts=document.querySelectorAll('.destenation__place_svg')
+
 btn.addEventListener('click',()=>{
 
     btnLine.classList.toggle('active')
     mobile_menu.classList.toggle('active')
 
 })
+
+hearts.forEach(like => {
+    like.addEventListener('click',()=>{
+        like.classList.toggle('active')
+        
+        
+        
+    })
+    
+});
